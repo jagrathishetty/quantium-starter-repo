b371392962998f98220f5c7b4e22a4cb2693625f
Jagrathi Shetty <jagrathishetty21@gmail.com>
2026-06-29 13:51:03 +0530

    Completed Task 5 Dash tests

--- test_app.py
+++ test_app.py
@@ -1,25 +1,14 @@
-from app import app
 
+from app import app
 
 def test_header_exists(dash_duo):
     dash_duo.start_server(app)
-
-    header = dash_duo.find_element("#header")
-
-    assert header is not None
-
+    assert dash_duo.find_element("#header")
 
 def test_graph_exists(dash_duo):
     dash_duo.start_server(app)
-
-    graph = dash_duo.find_element("#sales-chart")
-
-    assert graph is not None
-
+    assert dash_duo.find_element("#sales-chart")
 
 def test_region_picker_exists(dash_duo):
     dash_duo.start_server(app)
-
-    region_picker = dash_duo.find_element("#region-filter")
-
-    assert region_picker is not None
+    assert dash_duo.find_element("#region-filter")
